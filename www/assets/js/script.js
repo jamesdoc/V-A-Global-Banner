0ba53745654ef9a0505b05ab908c8861786b7c1a
James Doc <jamesdoc@gmail.com>
2014-04-23 17:58:43 +0100

    Hide one menu if another open

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -1,1 +1,11 @@
-document.getElementsByClassName('vam_global_banner')[0].className+=' js'
+function uncheck(el){ el.checked = false }
+
+var b = document.getElementsByClassName("vam_global_banner")[0],
+	st = document.getElementById("vam_global_site_navigation_toggle"),
+	ft = document.getElementById("vam_global_site_search_toggle");
+
+st.addEventListener("click", function(){uncheck(ft);});
+ft.addEventListener("click", function(){uncheck(st);});
+
+if (b.classList) { b.classList.add('js'); }
+else { b.className += ' ' + 'js'; }
